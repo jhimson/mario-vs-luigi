db0a9b81bb01ac48be1eba06ecc9eeb09c5a13fb
jhimson <jhimsonrayraboypamisa@gmail.com>
2022-05-23 10:41:16 -0400

    Initialize Luigi image object and set source of the image

--- app.js
+++ app.js
@@ -2,4 +2,8 @@ const canvas = document.querySelector('canvas');
 const c = canvas.getContext('2d');
 
 const marioImg = new Image();
+const luigiImg = new Image();
 marioImg.src = './images/mario-front.png';
+luigiImg.src = './images/luigi-front.png';
+
+
