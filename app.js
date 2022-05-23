e30074a4368d50a953f100e360eae2165b35a90b
jhimson <jhimsonrayraboypamisa@gmail.com>
2022-05-23 15:22:40 -0400

    Bug fix - movement left and right not stopping when pressed once

--- app.js
+++ app.js
@@ -54,31 +54,6 @@ window.addEventListener('DOMContentLoaded', (e) => {
     }
   } //! End of Player class ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>
 
-  //* Instantiate Mario object
-  const mario = new Player(
-    marioImg,
-    { x: 100, y: 100 },
-    { x: 0, y: 0 },
-    200,
-    200
-  );
-
-  //* Instantiate Luigi object
-  const luigi = new Player(
-    luigiImg,
-    { x: 1500, y: 100 },
-    { x: 0, y: 0 },
-    200,
-    200
-  );
-  startBtn.addEventListener('click', (e) => {
-    // marioBgMusic.play();
-    canvas.classList.remove('hidden');
-    startBtn.classList.add('hidden');
-    mario.position.y = 0;
-    luigi.position.y = 0;
-  });
-
   const keys = {
     mario: {
       right: {
@@ -98,78 +73,152 @@ window.addEventListener('DOMContentLoaded', (e) => {
     },
   };
 
-  //? A Function to constantly render the players and fireballs using the requestAnimationFrame built in function (Works the same as a recursion).
-  //? Invokes the update method for each player to constantly display the changes on the players' position.
-  const animate = () => {
-    window.requestAnimationFrame(animate);
-    c.clearRect(0, 0, canvas.width, canvas.height);
-    mario.update();
-    luigi.update();
+  //* Instantiate Mario object
+  const mario = new Player(
+    marioImg,
+    { x: 100, y: 100 },
+    { x: 0, y: 0 },
+    200,
+    200
+  );
 
-    //? This condition below will accelerate the left and right control for Mario object player by increasing and decreasing the x velocity. To avoid keep on pressing the left and right controls many times.
-    if (keys.mario.right.pressed) {
-      mario.velocity = { ...mario.velocity, x: 5 };
-    } else if (keys.mario.left.pressed) {
-      mario.velocity = { ...mario.velocity, x: -5 };
-    } else mario.velocity = { ...mario.velocity, x: 0 };
+  //* Instantiate Luigi object
+  const luigi = new Player(
+    luigiImg,
+    { x: 1500, y: 100 },
+    { x: 0, y: 0 },
+    200,
+    200
+  );
 
-    //? This condition below will accelerate the left and right control for Luigi object player by increasing and decreasing the x velocity. To avoid keep on pressing the left and right controls many times.
-    if (keys.luigi.right.pressed) {
-      luigi.velocity = { ...luigi.velocity, x: 5 };
-    } else if (keys.luigi.left.pressed) {
-      luigi.velocity = { ...luigi.velocity, x: -5 };
-    } else luigi.velocity = { ...luigi.velocity, x: 0 };
-  }; //! END OF ANIMATE FUNCTION ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>
+  startBtn.addEventListener('click', (e) => {
+    // marioBgMusic.play();
+    canvas.classList.remove('hidden');
+    startBtn.classList.add('hidden');
+    mario.position.y = 0;
+    luigi.position.y = 0;
 
-  animate();
+    //? A Function to constantly render the players and fireballs using the requestAnimationFrame built in function (Works the same as a recursion).
+    //? Invokes the update method for each player to constantly display the changes on the players' position.
+    const animate = () => {
+      window.requestAnimationFrame(animate);
+      c.clearRect(0, 0, canvas.width, canvas.height);
+      mario.update();
+      luigi.update();
 
-  window.addEventListener('keydown', ({ keyCode }) => {
-    console.log(keyCode);
-    switch (keyCode) {
-      case 37:
-        console.log('left');
-        keys.luigi.left.pressed = true;
+      //? This condition below will accelerate the left and right control for Mario object player by increasing and decreasing the x velocity. To avoid keep on pressing the left and right controls many times.
+      if (keys.mario.right.pressed) {
+        mario.velocity = { ...mario.velocity, x: 5 };
+      } else if (keys.mario.left.pressed) {
+        mario.velocity = { ...mario.velocity, x: -5 };
+      } else mario.velocity = { ...mario.velocity, x: 0 };
 
-        break;
-      case 40:
-        console.log('Down');
-        break;
-      case 39:
-        console.log('Right');
-        keys.luigi.right.pressed = true;
-        break;
+      //? This condition below will accelerate the left and right control for Luigi object player by increasing and decreasing the x velocity. To avoid keep on pressing the left and right controls many times.
+      if (keys.luigi.right.pressed) {
+        luigi.velocity = { ...luigi.velocity, x: 5 };
+      } else if (keys.luigi.left.pressed) {
+        luigi.velocity = { ...luigi.velocity, x: -5 };
+      } else luigi.velocity = { ...luigi.velocity, x: 0 };
+    }; //! END OF ANIMATE FUNCTION ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>
 
-      case 38:
-        console.log('Up');
-        luigi.velocity.y -= 20;
-        break;
+    animate();
+    window.addEventListener('keydown', ({ keyCode }) => {
+      console.log(keyCode);
+      switch (keyCode) {
+        case 37:
+          console.log('left');
+          keys.luigi.left.pressed = true;
 
-      case 17:
-        console.log('Luigi Shoot');
-        break;
+          break;
+        case 40:
+          console.log('Down');
+          break;
+        case 39:
+          console.log('Right');
+          keys.luigi.right.pressed = true;
+          break;
 
-      case 32:
-        console.log('shooot');
-        break;
-      case 65:
-        console.log('mario left');
-        keys.mario.left.pressed = true;
-        break;
-      case 83:
-        console.log('Down');
-        break;
-      case 68:
-        console.log('Right');
-        keys.mario.right.pressed = true;
-        break;
+        case 38:
+          console.log('Up');
+          luigi.velocity.y -= 20;
+          break;
 
-      case 87:
-        console.log('Up');
-        mario.velocity.y -= 20;
-        break;
+        case 17:
+          console.log('Luigi Shoot');
+          break;
 
-      default:
-        break;
-    }
-  }); //! END OF KEYDOWN EVENTLISTENER ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>
+        case 32:
+          console.log('shooot');
+          break;
+        case 65:
+          console.log('mario left');
+          keys.mario.left.pressed = true;
+          break;
+        case 83:
+          console.log('Down');
+          break;
+        case 68:
+          console.log('Right');
+          keys.mario.right.pressed = true;
+          break;
+
+        case 87:
+          console.log('Up');
+          mario.velocity.y -= 20;
+          break;
+
+        default:
+          break;
+      }
+    }); //! END OF KEYDOWN EVENTLISTENER ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>
+
+    window.addEventListener('keyup', ({ keyCode }) => {
+      switch (keyCode) {
+        case 37:
+          console.log('left');
+          luigi.velocity.x = 0;
+          keys.luigi.left.pressed = false;
+          break;
+        case 40:
+          console.log('Down');
+          break;
+        case 39:
+          console.log('Right');
+          keys.luigi.right.pressed = false;
+          break;
+        case 38:
+          console.log('Up');
+          break;
+
+        case 17:
+          console.log('Shoot');
+          break;
+
+        case 65:
+          console.log('left');
+          mario.velocity.x = 0;
+          keys.mario.left.pressed = false;
+          break;
+        case 83:
+          console.log('Down');
+
+          break;
+        case 68:
+          console.log('Right');
+          keys.mario.right.pressed = false;
+          break;
+
+        case 87:
+          console.log('Up');
+          break;
+
+        case 32:
+          console.log('Shoot');
+          break;
+
+        default:
+          break;
+      }
+    }); //! END OF KEYUP EVENTLISTENER ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>
+  }); //! END OF StartBtn CLICK EVENTLISTENER ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>
 }); //! END OF DOMContentLoaded eventListener ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>
