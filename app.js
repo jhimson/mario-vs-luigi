33015c5ef3a9bebf6bbcc961d3c36936cb7eac63
jhimson <jhimsonrayraboypamisa@gmail.com>
2022-05-23 13:31:42 -0400

    Fix bug - not redirecting to gameplay page and bg music not working

--- app.js
+++ app.js
@@ -1,45 +1,45 @@
 const canvas = document.querySelector('canvas');
 const c = canvas.getContext('2d');
+// Hide the canvas in the intialize load of the game.
 canvas.classList.add('hidden');
 const marioImg = new Image();
 const luigiImg = new Image();
 const marioFireball = new Image();
 const luigiFireball = new Image();
-const fireballSound = new Audio('./sound/fireball-sound.mp3');
-let marioBgMusic = new Audio('./sound/mario-bg-music.mp3');
+const fireballSound = new Audio('./sounds/fireball-sound.mp3');
+let marioBgMusic = new Audio('./sounds/mario-bg-music.mp3');
 marioImg.src = './images/mario-front.png';
 luigiImg.src = './images/luigi-front.png';
 marioFireball.src = './images/fireball.png';
 luigiFireball.src = './images/fireball-2.png';
+const startBtn = document.getElementById('start-button');
 
 canvas.width = innerWidth;
 canvas.height = innerHeight;
 
 window.addEventListener('DOMContentLoaded', (e) => {
   class Player {
-        constructor(url, position, velocity, height, width) {
-        this.url = url;
-        this.position = position;
-        this.velocity = velocity;
-        this.height = height;
-        this.width = width;
-        }
-
-        draw() {
-            c.drawImage(
-              this.url,
-              this.position.x,
-              this.position.y,
-              this.width,
-              this.height
-            );
-          }
-    
+    constructor(url, position, velocity, height, width) {
+      this.url = url;
+      this.position = position;
+      this.velocity = velocity;
+      this.height = height;
+      this.width = width;
     }
-}); //! End of Player class ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>
 
-//? Instantiate Mario object
-const mario = new Player(
+    draw() {
+      c.drawImage(
+        this.url,
+        this.position.x,
+        this.position.y,
+        this.width,
+        this.height
+      );
+    }
+  }
+
+  //? Instantiate Mario object
+  const mario = new Player(
     marioImg,
     { x: 100, y: 100 },
     { x: 0, y: 0 },
@@ -55,3 +55,10 @@ const mario = new Player(
     200,
     200
   );
+
+  startBtn.addEventListener('click', (e) => {
+    marioBgMusic.play();
+    canvas.classList.remove('hidden');
+    startBtn.classList.add('hidden');
+  });
+}); //! End of Player class ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>
